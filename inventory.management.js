15fa37912c3a0d4cbc4562d69e005f85e0f0fd8a
rajeeve12 <rajeevevishen21@gmal.com>
2024-09-15 12:38:48 -0400

    Create displayProductDetails function

--- inventory.management.js
+++ inventory.management.js
@@ -1,2 +1,10 @@
+// Task 1
 var inventory = ['name', 'price', 'quantity', 'lowStockLevel'];
 const lowestStock = inventory[inventory.length-1];
+// Task 2
+function displayProductDetails() {
+    const status = product.quantity <= product.lowStockLevel ? "Low Stock":"In Stock"
+    return `{product.name} is {status}`
+}
+//Task 3
+
