d6b13dc146350e8c5ba998701ca93cf2a30d6cb2
rajeeve12 <rajeevevishen21@gmal.com>
2024-09-15 15:15:16 -0400

    Create processSale function

--- inventory.management.js
+++ inventory.management.js
@@ -5,9 +5,32 @@ const lowestStock = inventory[inventory.length-1];
 function displayProductDetails() {
     const status = product.quantity <= product.lowStockLevel ? "Low Stock":"In Stock"
     return `{product.name} is {status}`
-}
-//Task 3
+};
+// Task 3
 function updateStock(){
     const unitsSold = unitsSold-quanitity <= product.lowStockLevel ? "Low Stock":"In Stock"
     console.log("low stock")
-}
+};
+// Task 4
+function checklowStock() {
+    for(let i <= inventory.length; i++)
+console.log("lowStockLevel");
+};
+// Task 5
+function calculateInventoryValue () {
+    return inventory.reduce ((totalValue, product) => {
+        return totalValue + (product.price * product.quantity)
+    },);
+};
+const inventoryTotalValue = calculateInventoryValue();
+console.log(`The total inventory value is $${inventoryTotalValue}`); 
+// Task 6
+function processSale(product, unitsSold){
+    var result = inventory.find(({name})=> name==product);
+    if(inventory.includes(product)) {
+        return `Product not in inventory`;
+    } else {
+        return updateStock(result, unitSold);
+    }
+    }
+console.log(processSale("Tablet",1));
